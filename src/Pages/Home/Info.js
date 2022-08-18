096f8137534a1c0832dbc24ba8629098774414e4
unknown <rased100@gmail.com>
2022-08-18 11:26:25 +0600

    infoCard

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import InfoCard from './InfoCard';
 import clock from '../../assets/icons/clock.svg';
+import marker from '../../assets/icons/marker.svg';
+import phone from '../../assets/icons/phone.svg';
 
 const info = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-            <InfoCard img={clock}></InfoCard>
-            <InfoCard img={clock}></InfoCard>
-            <InfoCard img={clock}></InfoCard>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+            <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" p="9:00am" cardTitle="Opening Hours" img={clock}></InfoCard>
+            <InfoCard bgClass="bg-accent" p="Mirpur-10, Dhaka-1216" cardTitle="Our Location" img={marker}></InfoCard>
+            <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" p="+8801716738111" cardTitle="Contact Us" img={phone}></InfoCard>
         </div>
     );
 };
