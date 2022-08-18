e5f08b461a20688457419d11f2759a60c62a57a3
unknown <rased100@gmail.com>
2022-08-18 14:04:17 +0600

    Review

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -6,7 +6,7 @@ import phone from '../../assets/icons/phone.svg';
 
 const info = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
             <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" p="9:00am" cardTitle="Opening Hours" img={clock}></InfoCard>
             <InfoCard bgClass="bg-accent" p="Mirpur-10, Dhaka-1216" cardTitle="Our Location" img={marker}></InfoCard>
             <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" p="+8801716738111" cardTitle="Contact Us" img={phone}></InfoCard>
