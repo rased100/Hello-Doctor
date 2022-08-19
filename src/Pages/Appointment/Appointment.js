8eee43a475c98ae2ac465f30df34324e08876ffe
unknown <rased100@gmail.com>
2022-08-20 00:31:59 +0600

    home page done

---
+++ src/Pages/Appointment/Appointment.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Footer from '../Shared/Footer';
+
+const Appointment = () => {
+    return (
+        <Footer></Footer>
+    );
+};
+
+export default Appointment;
