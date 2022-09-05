ccd46959f2d60a47b3be62e187b9f5ccebecc847
unknown <rased100@gmail.com>
2022-09-05 23:16:30 +0600

    done

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import chair from '../../assets/images/chair.png';
 import PrimaryButton from '../Shared/PrimaryButton';
 import './Banner.css';
@@ -11,7 +12,7 @@ const Banner = () => {
                 <div>
                     <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <PrimaryButton>Get started</PrimaryButton>
+                    <Link to="/appointment"><PrimaryButton>Get started</PrimaryButton></Link>
                 </div>
             </div>
         </div>
--- src/Pages/Home/Treatment.js
+++ src/Pages/Home/Treatment.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import treatment from '../../assets/images/treatment.png';
 import PrimaryButton from '../Shared/PrimaryButton';
 
@@ -10,7 +11,7 @@ const Treatment = () => {
                 <div className='w-2/5 text-left'>
                     <h1 className="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
                     <p className="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
-                    <PrimaryButton>Get Started</PrimaryButton>
+                    <Link to="/appointment"><PrimaryButton>Get started</PrimaryButton></Link>
                 </div>
             </div>
         </div>
